0c3ec52e464c9f47d590da2434c8172e1fb95d1c
onebitlab <onebitresearch@gmail.com>
2025-06-25 20:35:44 +0700

    updated

--- config_sample.py
+++ config_sample.py
@@ -5,7 +5,7 @@ api_key = 'YOUR_BYBIT_API_KEY'          # Your Bybit API Key
 api_secret = 'YOUR_BYBIT_SECRET_KEY'    # Your Bybit Secret Key
 
 # Trading configuration
-pair = 'ALT/USDT'                     # Trading pair, e.g.: 'ALT/USDT'
+pair = 'ALT/USDT'                       # Trading pair, e.g.: 'ALT/USDT'
 tokens_for_sale = '170'                 # Amount of tokens to sell
 price_offset = '1.0'                    # Percentage below market price (e.g., '1.0' means 1% below)
 
